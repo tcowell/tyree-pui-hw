1ba1949c08692b698250dd43111ebfd1793b35dc
tcowell <84730021+tcowell@users.noreply.github.com>
2022-09-20 16:25:47 -0400

    finished pricing functionality

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -1,3 +1,5 @@
+// Define objects for glazing options
+
 const keepOriginal = {
     optionName: "Keep Original",
     glazingPrice: 0
@@ -18,6 +20,9 @@ const doubleChocolate = {
     glazingPrice: 1.5
 };
 
+
+// Use loop to set drop down options for glazing
+
 const glazingSelect = [keepOriginal, sugarMilk, vanillaMilk, doubleChocolate];
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -30,6 +35,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     glazingOptions.innerHTML = options;
 });
+
+
+
+// Use loop to set drop down options for pack size
 
 const packSelect = [1, 3, 5, 10]
 
@@ -44,13 +53,54 @@ document.addEventListener('DOMContentLoaded', function() {
     packOptions.innerHTML = options;
 });
 
-function glazingChange(element) {
-    const priceChange = Number(element.value);
-    console.log(priceChange);
 
-    let newPrice = priceChange + 2.49;
-    console.log(newPrice);
 
-    let price = document.querySelector('#price');
-    price.innerHTML = '$ ' + newPrice;
+// Create object for price caluclations
+const orderSummary = {
+    basePrice: 2.49,
+    glazingPrice: 0,
+    packPrice: 1
 };
+
+// onChange function for glazing drop down
+function glazingChange(element) {
+    
+    //collect the glazing price from the drop down menu
+    const priceChange = Number(element.value);
+
+    //update orderSummary glazingPrice to be the glazing price from the drop down
+    orderSummary.glazingPrice = priceChange;
+
+    //call the update price function using the updated orderSummary object
+    updatePrice(orderSummary);
+};
+
+
+// onChange function for pack size drop down
+function packChange(element) {
+    
+    //collect the pack price from the drop down menu
+    const priceChange = Number(element.value);
+
+    //update orderSummary packPrice to be the pack size from the drop down
+    orderSummary.packPrice = priceChange;
+
+    //call the update price function using the updated orderSummary object
+    updatePrice(orderSummary);
+};
+
+
+// function that updates the price in the DOM
+function updatePrice(order) {
+
+    //select the price element in the DOM
+    let price = document.querySelector('#price');
+
+    //caluclate the final price to be displayed using values in orderSummary
+    let finalPrice = (order.basePrice + order.glazingPrice) * order.packPrice;
+    finalPrice = finalPrice.toFixed(2);
+
+    //update DOM to display new price
+    price.innerHTML = '$ ' + finalPrice;
+
+}
